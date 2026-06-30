08197149fa375edc9ac16875fd3e16ad5458035c
yym0110 <hi.myshawn@gmail.com>
2026-06-29 21:42:12 -0700

    Added Intake and pidDrive hw

---
+++ TeamCode/src/main/java/robot/intake.java
@@ -0,0 +1,18 @@
+package robot;
+
+import org.firstinspires.ftc.teamcode.utils.priority.PriorityMotor;
+
+public PriorityMotor intakeMotor;
+
+public class intake {
+    //some priority queue
+    intakeMotor.//set motor power(setPower(true))
+}
+
+public int setPower(boolean intakeRequest){
+    int power = 0;
+    if(intakeRequest){
+        power = 1;
+    }
+    return power;
+}

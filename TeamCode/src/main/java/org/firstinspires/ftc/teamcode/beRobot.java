fdc897094455eaa5c6814f86f0f449197c0bdd48
yym0110 <hi.myshawn@gmail.com>
2026-07-14 18:11:37 +0800

    Updated some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/beRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/beRobot.java
@@ -1,7 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.sensors.Sensors;
+import org.firstinspires.ftc.teamcode.subsystems.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.intake.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.park.Park;
+import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
+
 public class beRobot {
     public HardwareMap hardwareMap;
+    public HardwareQueue hardwareQueue;
 
     public Sensors sensors;
     public Drivetrain drivetrain;
@@ -9,4 +18,7 @@ public class beRobot {
     public Deposit deposit;
     public Park park;
 
+    public void update(){
+
+    }
 }

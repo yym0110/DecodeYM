bb1477069cb242e1f6c2c1466d1c17511d8ced23
yym0110 <hi.myshawn@gmail.com>
2026-07-14 12:19:52 +0800

    Created beRobot.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/beRobot.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class beRobot {
+    public HardwareMap hardwareMap;
+
+    public Sensors sensors;
+    public Drivetrain drivetrain;
+    public Intake intake;
+    public Deposit deposit;
+    public Park park;
+
+}

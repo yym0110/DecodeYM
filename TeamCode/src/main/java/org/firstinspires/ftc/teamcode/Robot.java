77731f07b8f4b53aef914f66125d9fe20e8fba8a
yym0110 <hi.myshawn@gmail.com>
2026-07-15 11:10:57 +0800

    Updated Robot stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -11,6 +11,8 @@ import org.firstinspires.ftc.teamcode.subsystems.park.Park;
 import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
 import org.firstinspires.ftc.teamcode.vision.Vision;
 
+import java.util.function.BooleanSupplier;
+
 public class Robot {
     public HardwareMap hardwareMap;
     public HardwareQueue hardwareQueue;
@@ -19,6 +21,8 @@ public class Robot {
     public Drivetrain drivetrain;
     public Intake intake;
     public Park park;
+
+    private BooleanSupplier stopChecker = null;
 
     public Robot(HardwareMap hardwareMap, Vision vision){
         this.hardwareMap = hardwareMap;
@@ -34,10 +38,14 @@ public class Robot {
     public void update(){
         START_LOOP();
 
+        if (this.stopChecker != null && this.stopChecker.getAsBoolean()) return;
+
         sensors.update();
         drivetrain.update();
         intake.update();
         park.update();
+
+        if (this.stopChecker != null && this.stopChecker.getAsBoolean()) return;
 
         hardwareQueue.update();
 
